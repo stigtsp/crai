43ef0e60e5020ec4bdceaa7bc2d689d5d82b9b43
Chloé <rightfold+ck@gmail.com>
2019-12-21 21:47:58 +0100

    Don’t build documentation
    It takes too long.

--- default.nix
+++ default.nix
@@ -9,7 +9,6 @@ in
             pkgs.raku-nix.DBIish
             pkgs.raku-nix.Inline-Perl5
             pkgs.raku-nix.JSON-Fast
-            pkgs.raku-nix.Pod-To-HTML
             pkgs.raku-nix.Terminal-ANSIColor
         ];
         preInstallPhase = ''
@@ -30,15 +29,5 @@ in
                 --prefix PATH : ${pkgs.gnutar}/bin \
                 --prefix PATH : ${pkgs.jq}/bin \
                 --prefix PATH : ${pkgs.rsync}/bin
-
-            (
-                export PERL6LIB=$(< $out/PERL6LIB)
-                for module in $(find lib -name '*.pm6'); do
-                    html=$out/share/doc/''${module#lib/}
-                    html=''${html%.pm6}.html
-                    mkdir --parents $(dirname $html)
-                    raku --doc=HTML "$module" > "$html"
-                done
-            )
         '';
     }
